f18870a84950855040bda5843996323e9de38d82
corozco <carlos.orozco@techedgegroup.com>
2021-10-11 23:55:50 +0200

    feat: added bootstrap tests

--- src/test/suite/index.ts
+++ src/test/suite/index.ts
@@ -6,7 +6,7 @@ export function run(): Promise<void> {
   // Create the mocha test
   const mocha = new Mocha({
     ui: 'tdd',
-    timeout: '30s',
+    timeout: '5s',
     color: true,
   });
   //   mocha.useColors(true);
